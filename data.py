b0382fe43c44afacc661ab8a8c85c23eccf360eb
Rashneema <79697800+Rashneema@users.noreply.github.com>
2023-11-26 13:38:07 +0400

    Update data.py

--- data.py
+++ data.py
@@ -5,7 +5,7 @@ import streamlit as st
 # Define a function 'app()' which accepts 'car_df' as an input.
 def app(car_df):
     st.header("View Data")
-    with st.expander("View Dataset"):
+    with st.beta_expander("View Dataset"):
         st.table(car_df)
 
 
@@ -15,7 +15,7 @@ def app(car_df):
 
      # ADD NEW CODE FROM HERE
     # Add a subheader and create three columns. Store the columns in two separate variables.
-    beta_col1, beta_col2, beta_col3 = st.columns(3)
+    beta_col1, beta_col2, beta_col3 = st.beta_columns(3)
 
     # Add a checkbox in the first column. Display the column names of 'car_df' on the click of checkbox.
     with beta_col1:
